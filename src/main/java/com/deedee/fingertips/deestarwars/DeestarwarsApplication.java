dd3b53f37b62f1cc65178619a845021bb7bb687a
adedamola <adedamola47@gmail.com>
2019-11-29 09:39:20 +0100

    App and database setup

---
+++ src/main/java/com/deedee/fingertips/deestarwars/DeestarwarsApplication.java
@@ -0,0 +1,13 @@
+package com.deedee.fingertips.deestarwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeestarwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeestarwarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deedee/fingertips/deestarwars/DeestarwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deedee.fingertips.deestarwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeestarwarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
