71169847448eb0d105b7b052b33cee50ffbb7896
adedamola <adedamola47@gmail.com>
2019-12-15 21:53:41 +0100

    refactor swagger docs

--- src/main/java/com/deedee/fingertips/deestarwars/controllers/HomeController.java
+++ src/main/java/com/deedee/fingertips/deestarwars/controllers/HomeController.java
@@ -1,14 +1,16 @@
 package com.deedee.fingertips.deestarwars.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @ApiOperation(value = "Home of Stars")
     @RequestMapping("/")
     public String home()
     {
-        return "Welcome to DeeStarwars";
+        return "Welcome to DeeStarwars!!!";
     }
 }
--- src/main/java/com/deedee/fingertips/deestarwars/controllers/MovieController.java
+++ src/main/java/com/deedee/fingertips/deestarwars/controllers/MovieController.java
@@ -3,6 +3,7 @@ package com.deedee.fingertips.deestarwars.controllers;
 import com.deedee.fingertips.deestarwars.models.Movie;
 import com.deedee.fingertips.deestarwars.models.PeopleQueryParams;
 import com.deedee.fingertips.deestarwars.interfaces.IMovieService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ public class MovieController
     @Autowired
     public IMovieService movieService;
 
+    @ApiOperation(value = "Find a Movie with list of characters using Movie ID")
     @RequestMapping(method = RequestMethod.POST, value = "/find", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public Movie findFilmWithCharacters(@RequestParam("movieId") int movieId, @RequestBody PeopleQueryParams peopleQueryParams) throws Exception
