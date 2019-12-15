f4de3a4326a584e11c151b0d4e646e820f815613
adedamola <adedamola47@gmail.com>
2019-12-15 22:09:19 +0100

    add get method to landing controller

--- src/main/java/com/deedee/fingertips/deestarwars/controllers/HomeController.java
+++ src/main/java/com/deedee/fingertips/deestarwars/controllers/HomeController.java
@@ -2,13 +2,14 @@ package com.deedee.fingertips.deestarwars.controllers;
 
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
     @ApiOperation(value = "Home of Stars")
-    @RequestMapping("/")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public String home()
     {
         return "Welcome to DeeStarwars!!!";
